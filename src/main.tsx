4a3aa11be704a6d2d7e6c63c4325f2710018db7b
YarielZC <yarielzamoracueto@gmail.com>
2026-01-08 14:49:40 -0500

    Use AuthProvider at  app

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import './styles/variables.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router';
+import AuthProvider from './context/AutContext/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
