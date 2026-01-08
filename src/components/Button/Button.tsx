016dbb91ce073f6ee5084e8c674cc4918554e908
YarielZC <yarielzamoracueto@gmail.com>
2026-01-08 17:38:10 -0500

    Add submit button variant

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import type { MouseEventHandler, ReactNode } from 'react'
 import './Button.css'
 import { cn } from '../../lib/utils'
 
-type variantType = 'primary' | 'secondary' 
+type variantType = 'primary' | 'secondary' | 'submit'
 
 export default function Button({ children, variant, className, onClick}: {children: ReactNode, variant?: variantType | undefined, className?: string | undefined, onClick: MouseEventHandler<HTMLButtonElement>}) {
 
@@ -11,10 +11,11 @@ export default function Button({ children, variant, className, onClick}: {childr
     <button
       onClick={onClick}
       className={cn(
-        className,
+        `${className} cursor-pointer`,
         !variant && 'bg-red-500',
 
-        variant == 'primary' && 'bg-[var(--button-color)] text-white py-[0.4rem] px-4 rounded-lg font-semibold hover:bg-[var(--button-color-hover)]'
+        variant == 'primary' && 'bg-[var(--button-color)] text-white py-[0.4rem] px-4 rounded-lg font-semibold hover:bg-[var(--button-color-hover)]',
+        variant == 'submit' && 'bg-[var(--button-color)] text-white py-3 px-4 rounded-lg font-semibold hover:bg-[var(--button-color-hover)]'
       )}
     >
       {children}
