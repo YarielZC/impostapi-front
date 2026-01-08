80b99b5a3468e2b7d13ae9e04162ae182f5992b8
YarielZC <yarielzamoracueto@gmail.com>
2026-01-08 15:17:55 -0500

    Header buttons adapt to authenticated global state

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,17 @@ import Button from "../Button/Button";
 import './Header.css'
 import * as LogoWebp from '../../assets/Logo.webp'
 import Href from "../Href/Href";
+import * as PhotoUser from '../../assets/PhotoIcons/PhotoUser.webp'
+import { useAuth } from '../../context/AutContext/useAuth';
 
 export default function Header() {
+  const {isAuthenticated} = useAuth()
+
   return (
-    <header className='flex w-full border-b-2 border-[var(--border-color)] items-center justify-between p-5'>
+    <header className='flex w-full border-b-2 border-[var(--border-color)] items-center justify-between p-5 h-20'>
 
       <div className='flex items-center justify-center gap-4'>
-        <img className='w-10' src={LogoWebp.default} alt="" />
+        <img className='w-10' src={LogoWebp.default} alt='Logotipo de ImpostAPI' />
         <h1 className='font-bold text-2xl'>ImpostAPI</h1>
       </div>
 
@@ -18,9 +22,25 @@ export default function Header() {
         <Href to={'/docs'}>Docs</Href>
       </nav>
 
+
+    
+
       <div className='flex items-center justify-center gap-8'>
-        <Href to={'/login'}>Login</Href>
-        <Button onClick={() => {return}} variant='primary'>Sign Up</Button>
+        { isAuthenticated ?
+        (<>
+          <div className='flex flex-col'>
+            <p className='text-md font-semibold text-white'>Username</p>
+            <p className='text-[var(--secondary-text-color)] text-sm'>Free Plan</p>
+          </div>
+          <img className='w-10' src={PhotoUser.default} alt='Imagen generica de usuario' />
+        </>)
+        :
+        (<>
+          <Href to={'/login'}>Login</Href>
+          <Href to='/register'>
+            <Button onClick={() => {return}} variant='primary'>Sign Up</Button>
+          </Href>
+        </>)}
       </div>
     </header>
   )
