dc86d02287fac86e4e2a99fa2e50f0b866f81f1b
YarielZC <yarielzamoracueto@gmail.com>
2026-01-09 02:21:46 -0500

    Add StyledLabel component

---
+++ src/components/StyledLabel/StyledLabel.tsx
@@ -0,0 +1,10 @@
+import './StyledLabel.css'
+import { IconPointFilled, type ReactNode } from '@tabler/icons-react'
+export default function StyledLabel({ children, className, textClassName, iconClassName }: {children: ReactNode, className?: string, textClassName?: string, iconClassName?: string}) {
+  return (
+    <div className={`flex w-fit gap-1 items-center py-1.5 px-4 rounded-full bg-[var(--dark-mode-card-form-color)] border-2 border-[var(--dark-mode-border-color)] ${className}`}>
+      <IconPointFilled className={`w-fit ${iconClassName}`}></IconPointFilled>
+      <p className={`mr-2 ${textClassName}`}>{children}</p>
+    </div>
+  )
+}
