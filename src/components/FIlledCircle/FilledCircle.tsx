e8df9f4b6798d5fe0ff81a65d9c5f2b7ed8d19ba
YarielZC <yarielzamoracueto@gmail.com>
2026-01-09 02:21:23 -0500

    Add FilledCircle component

---
+++ src/components/FIlledCircle/FilledCircle.tsx
@@ -0,0 +1,5 @@
+import './FilledCircle.css'
+
+export default function FilledCircle({ className, color }: { className?: string, color: string }) {
+  return <div className={`${className} w-4 h-4 bg-[${color}] rounded-full`}/>
+}
