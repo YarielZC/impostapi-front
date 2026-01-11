73940c36a4e9dbdc5e50851996a8cd071aaf7bbd
YarielZC <yarielzamoracueto@gmail.com>
2026-01-10 20:29:54 -0500

    Fix error in ProtectedRoute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -6,8 +6,8 @@ import { type ReactNode } from 'react';
 export default function ProtectedRoute ({ children }: {children: ReactNode}) {
   const { isAuthenticated } = useAuth();
 
-    if (!isAuthenticated) {
-    return <Navigate to='login'/>
+  if (!isAuthenticated) {
+    return <Navigate to='/login'/>
   }
 
   return children;
