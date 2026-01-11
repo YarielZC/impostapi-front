b186ce5c24037eb98d5dcd5f6b224bb08fc32dc2
YarielZC <yarielzamoracueto@gmail.com>
2026-01-10 20:28:46 -0500

    Add ButtonPanel component

---
+++ src/components/ButtonPanel/ButtonPanel.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState, type ReactNode } from 'react'
+import Href from '../Href/Href'
+import './ButtonPanel.css'
+import { useLocation } from 'react-router'
+
+export default function ButtonPanel( {to, children, icon, className}: {to: string, children:ReactNode, icon: ReactNode, className?: string} ) {
+  const [isActive, setIsActive] = useState(false)
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname == to) {
+      setIsActive(true)
+      return
+    }
+    setIsActive(false)
+  }, [location, to])
+
+  return (
+    <Href className={`${className}`} to={to}>
+      <button
+        className='flex gap-2.5 py-3 px-4 rounded-lg w-full'
+        style={{
+          color: `${isActive ? `#ffffff` : undefined}`,
+          backgroundColor: `${isActive ? `#262626` : 'transparent'}`
+        }}
+      >
+        <div style={{
+          color: `${isActive ? `var(--text-resalt-color)`: `var(--secondary-text-color)`}`
+        }}>
+          {icon}
+        </div>
+        {children}
+      </button>
+    </Href>
+  )
+}
