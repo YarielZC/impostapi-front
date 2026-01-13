ddefbae08091d25dceae0391872845ee235e9b37
YarielZC <yarielzamoracueto@gmail.com>
2026-01-12 22:06:28 -0500

    Add posibilite of create a styledlabel without icon

--- src/components/StyledLabel/StyledLabel.tsx
+++ src/components/StyledLabel/StyledLabel.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import { darkenHexColor } from '../../logic/colorTools'
 import './StyledLabel.css'
 import { IconPointFilled, type ReactNode } from '@tabler/icons-react'
@@ -6,8 +5,7 @@ import { IconPointFilled, type ReactNode } from '@tabler/icons-react'
 type colorType = `#${string}`
 type roundedType = 'full' | 'medium'
 
-export default function StyledLabel({ colorText, colorIcon, children, className, textClassName, iconClassName, roundedType }: {colorText: colorType, colorIcon: colorType, children: ReactNode, className?: string, textClassName?: string, iconClassName?: string, roundedType?: roundedType}) {
-  console.log(darkenHexColor(colorText, 0.3))
+export default function StyledLabel({ colorText, colorIcon, children, className, textClassName, iconClassName, roundedType, notIcon }: {colorText: colorType, colorIcon?: colorType, children: ReactNode, className?: string, textClassName?: string, iconClassName?: string, roundedType?: roundedType, notIcon?: boolean}) {
 
   const roundedValue = () => {
     if (roundedType) {
@@ -18,20 +16,23 @@ export default function StyledLabel({ colorText, colorIcon, children, className,
   }
 
   return (
-    <div className={`${className} flex w-fit gap-1 items-center py-1.5 px-4 roundeds-full  border-2 `}
+    <div className={`${className} h-fit flex w-fit gap-1 items-center py-1.5 px-2 border-2 `}
     style={{
       borderColor: darkenHexColor(colorText, 0.6),
       backgroundColor: darkenHexColor(colorText, 0.85),
       borderRadius: roundedValue()
     }}
     >
-      <IconPointFilled 
+      {!notIcon && <IconPointFilled 
         className={`${iconClassName}`}
         style={{color: colorIcon}}
-      ></IconPointFilled>
+      ></IconPointFilled>}
       <p 
-        className={`mr-2 ${textClassName}`}
-        style={{color: colorText}}
+        className={`flex items-center h-fit leading-none ${textClassName}`}
+        style={{
+          color: colorText,
+          marginRight: !notIcon ? '0.5rem' : 0
+        }}
       >{children}</p>
     </div>
   )
