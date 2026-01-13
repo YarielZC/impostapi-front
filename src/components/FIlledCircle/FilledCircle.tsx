34cfa880c966c765b226e7b071c87bc1bbb41f86
YarielZC <yarielzamoracueto@gmail.com>
2026-01-12 22:07:18 -0500

    Add width prop in filledcircle component

--- src/components/FIlledCircle/FilledCircle.tsx
+++ src/components/FIlledCircle/FilledCircle.tsx
@@ -1,5 +1,16 @@
+import { cn } from '../../lib/utils'
 import './FilledCircle.css'
 
-export default function FilledCircle({ className, color }: { className?: string, color: string }) {
-  return <div className={`${className} w-4 h-4 rounded-full`} style={{backgroundColor: color}}/>
+export default function FilledCircle({ className, color, width }: { className?: string, color: string, width?: string | number }) {
+  return <div 
+            className={cn(
+              `${className} rounded-full aspect-square`,
+              !width &&  'w-4 h-4',
+            )  
+            } 
+            style={{
+              backgroundColor: color,
+              width: `${width ? width : ''}`
+            }}
+          />
 }
