68ac30888678b789074efa2b43d5a5abe8fabdcc
YarielZC <yarielzamoracueto@gmail.com>
2026-01-13 17:18:19 -0500

    Migrate description code function to statuscodelabel component

--- src/components/ConsoleText/ConsoleText.tsx
+++ src/components/ConsoleText/ConsoleText.tsx
@@ -2,22 +2,6 @@ import StatusCodeLabel from '../StatusCodeLabel/StatusCodeLabel';
 import StyledLabel from '../StyledLabel/StyledLabel';
 
 export default function ConsoleText({timestamp, projectName, endpointName, method, code, url, delay}: {timestamp?: string, projectName: string, endpointName: string, method: string, code: string | number, url: string, delay: string | number}) {
-  
-  const addDescriptionCode = (): {text: string, color: `#${string}`} => {
-    if (code == 200) {
-      return {text: `${code} OK`, color: '#24d364'}
-    }
-    if (code == 201) {
-      return {text: `${code} Created`, color: '#24d364'}
-    }
-    if (code == 204) {
-      return {text: `${code} No content`, color: '#c3cada'}
-    }
-
-
-    return {text: `${code}`, color: '#e46b15'}
-  }
-
 
   return (
     <div className='px-3 py-1.5 text-sm font-mono flex text-[#c3cada]c flex-col gap-0 w-full hover:bg-[#262626]'>
@@ -26,7 +10,7 @@ export default function ConsoleText({timestamp, projectName, endpointName, metho
           {'> '}
           {timestamp} 
           
-          <span className='text-red-400 flex items-center gap-2'>{`[${projectName}/ ${endpointName}]:`}
+          <span className='text-[#a49cff] flex items-center gap-2'>{`[${projectName}/ ${endpointName}]:`}
           </span>
         </p>
         <StyledLabel 
@@ -37,10 +21,7 @@ export default function ConsoleText({timestamp, projectName, endpointName, metho
       </div>
       <div className='px-0 py-1.5'>
         <div className='text-white flex items-center gap-2'>
-          <StatusCodeLabel
-            colorText={addDescriptionCode().color}
-            notIcon
-          >{addDescriptionCode().text}</StatusCodeLabel>
+          <StatusCodeLabel size='xs' roundedType='medium' code={code}/>
           <p>{url}</p>
         </div>
       </div>
--- src/components/StatusCodeLabel/StatusCodeLabel.tsx
+++ src/components/StatusCodeLabel/StatusCodeLabel.tsx
@@ -1,17 +1,31 @@
-import type { ReactNode } from 'react'
+import { cn } from '../../lib/utils'
 import StyledLabel from '../StyledLabel/StyledLabel'
 import './StatusCodeLabel.css'
 
-type colorType = `#${string}`
+type roundedTypes = 'full' | 'medium'
+type sizeType = 'xs' | 'sm' | 'base' | 'lg' | 'xl' 
+export default function StatusCodeLabel({code, size, roundedType}: { code: string | number, size: sizeType, roundedType: roundedTypes}) {
+  const addDescriptionCode = (): {text: string, color: `#${string}`} => {
+    if (code == 200) {
+      return {text: `${code} OK`, color: '#24d364'}
+    }
+    if (code == 201) {
+      return {text: `${code} Created`, color: '#24d364'}
+    }
+    if (code == 204) {
+      return {text: `${code} No content`, color: '#c3cada'}
+    }
 
-export default function StatusCodeLabel({children, colorText, notIcon = false}: {children: ReactNode, colorText: colorType, notIcon: boolean}) {
 
+    return {text: `${code}`, color: '#e46b15'}
+  }
 
   return (
     <StyledLabel 
-      colorText={colorText}
-      notIcon={notIcon}
-      textClassName='text-xs'
-    >{children}</StyledLabel> 
+      colorText={addDescriptionCode().color}
+      notIcon
+      textClassName={cn(`text-${size} font-mono`)}
+      roundedType={roundedType}
+    >{addDescriptionCode().text}</StyledLabel> 
   )
 }
